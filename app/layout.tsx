c516e980470553e5e19330b9a7ebee8543763982
fred <fredjuma8@gmail.com>
2024-07-27 02:19:38 +0300

    final development touches

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const font = Poppins(
   { subsets: ["latin"],
@@ -20,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Analytics />
+        {children}
+        
+        </body>
     </html>
   );
 }
